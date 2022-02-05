4fb01b344f825db56a870fbb5e3c45a33bf338c5
Ondra Červenka <gogo.lejzr@gmail.com>
2022-02-06 00:19:07 +0100

    added timestamp to example

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 from sys import argv
 from time import sleep
+from datetime import datetime
 from pymhz.mhz19b import MHZ19B
-
 
 with MHZ19B(argv[1]) as sensor:
     sensor.enable_autocalibration()
     while True:
         response = sensor.read_concentration()
-        print(f"CO2: {response.concentration} ppm")
-        sleep(1.0)
+        print(f"[{datetime.now().isoformat()}] CO2: {response.concentration} ppm")
+        sleep(0.9)
